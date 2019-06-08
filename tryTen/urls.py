e48ef8f3a5ab67423f626efd73f8edc3006f1835
marvin evins <maevins@cps.edu>
2019-06-08 15:30:09 -0500

    working on block content

--- tryTen/urls.py
+++ tryTen/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from profiles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', views.home, name='home'),
+
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
